f3cc732eda4f321da44e75b84571a81ce81db6ba
softwaresuchitraind <software.suchitra@gmail.com>
2024-11-24 20:10:02 +0530

    Oldest Employee with Maximum Salary

--- javainterview/src/javainterview/EmpData1.java
+++ javainterview/src/javainterview/EmpData1.java
@@ -17,10 +17,16 @@ public class EmpData1
 		Employee1 employee7= new Employee1("xyz", 52, 10000.0);
 		List<Employee1> list = Arrays.asList(employee1,employee2,employee3,employee4,employee5,employee6,employee7);
 		
+		/*
 		//Oldest Employee with Minimum Salary
 		list.stream().sorted(Comparator.comparing(Employee1::getEmpAge).reversed()
 				.thenComparing(Employee1::getEmpSal))
-				.findFirst().ifPresent(System.out::println);
+				.findFirst().ifPresent(System.out::println);*/
+		
+		//Oldest Employee with Maximum Salary
+		list.stream()
+		.max(Comparator.comparing(Employee1::getEmpAge)
+				.thenComparing(Employee1::getEmpSal)).ifPresent(System.out::println);
 				
 	}
 }
