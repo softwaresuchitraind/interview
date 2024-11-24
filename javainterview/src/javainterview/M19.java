63dff4b855d6c1af19c3150e3221c25544aab7f8
softwaresuchitraind <software.suchitra@gmail.com>
2024-11-24 22:35:50 +0530

    Finding Duplicate Elements using java 8

--- javainterview/src/javainterview/M19.java
+++ javainterview/src/javainterview/M19.java
@@ -1,9 +1,26 @@
 package javainterview;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+/*
+ * Find only the duplicate elements using java 8 stream
+List<Integer> list = Arrays.asList(10,10,20,30,20,15,40,5);
+ * */
 public class M19
 {
 	public static void main(String[] args)
 	{
-		
+		List<Integer> list = Arrays.asList(10,10,20,30,20,15,40,5);
+		list.stream()
+			.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+			.entrySet()
+			.stream()
+			.filter(x->x.getValue()>1)
+			.map(Map.Entry::getKey)
+			.forEach(System.out::println);
 	}
 }
