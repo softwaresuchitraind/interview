f0912c3a6ebda066a876533a96f32ba7f6cf646e
softwaresuchitraind <software.suchitra@gmail.com>
2024-12-09 07:54:50 +0530

    Removing duplicates from an Array using java 8 Modified

--- javainterview/src/javainterview/M37.java
+++ javainterview/src/javainterview/M37.java
@@ -4,18 +4,22 @@ package javainterview;
  * Removing duplicates from an Array using java 8
  * */
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class M37
 {
 	public static void main(String[] args)
 	{
-		int arr[] = {10,15,8,49,25,98,98,32,15};
+		//int arr[] = {10,15,8,49,25,98,98,32,15};
 		
-		Arrays.stream(arr).forEach(x->System.out.print(x+ " "));
+		//Arrays.stream(arr).forEach(x->System.out.print(x+ " "));
 		
-		System.out.println("Array elements after distict");
+		//System.out.println("Array elements after distict");
 		
-		Arrays.stream(arr).distinct().forEach(x->System.out.print(x+ " "));
+		//Arrays.stream(arr).distinct().forEach(x->System.out.print(x+ " "));
+		
+		ArrayList<Integer> list = new ArrayList<Integer>(Arrays.asList(10,15,8,49,25,98,98,32,15));
+		list.stream().distinct().forEach(System.out::println);
 	}
 }
