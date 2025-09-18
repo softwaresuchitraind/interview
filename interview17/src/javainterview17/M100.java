7017575023db15145b7ee7933904c968f6d15566
softwaresuchitraind <software.suchitra@gmail.com>
2025-09-18 20:24:30 +0530

    How to calculate count,sum,min,max and average of marks in just one stream operation ?

--- interview17/src/javainterview17/M100.java
+++ interview17/src/javainterview17/M100.java
@@ -1,6 +1,8 @@
-package javainterview;
+package javainterview17;
 
+import java.util.IntSummaryStatistics;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /*
  * 
@@ -10,7 +12,14 @@ public class M100
 {
 	public static void main(String[] args)
 	{
-		List<Object> Marks = List.of(85,90,78,95,88);
+		List<Integer> Marks = List.of(85,90,78,95,88);//java 9
+		IntSummaryStatistics stats = Marks.stream().collect(Collectors.summarizingInt(Integer::intValue));
+		
+		System.out.println("Counts: "+stats.getCount());
+		System.out.println("Sum: "+stats.getSum());
+		System.out.println("Min: "+stats.getMin());
+		System.out.println("Max: "+stats.getMax());
+		System.out.println("Average: "+stats.getAverage());
 		
 	}
 }
