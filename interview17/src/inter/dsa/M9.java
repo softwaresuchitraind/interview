3e04db33fbe374bd372441e30b35eb29c7a3a712
softwaresuchitraind <software.suchitra@gmail.com>
2026-01-30 08:08:04 +0530

    How Many Numbers Are Smaller Than the Current Number

---
+++ interview17/src/inter/dsa/M9.java
@@ -0,0 +1,55 @@
+package inter.dsa;
+
+import java.util.Arrays;
+
+/*
+ * 1365. How Many Numbers Are Smaller Than the Current Number
+Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+
+Return the answer in an array.
+
+ 
+
+Example 1:
+
+Input: nums = [8,1,2,2,3]
+Output: [4,0,1,1,3]
+Explanation: 
+For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3). 
+For nums[1]=1 does not exist any smaller number than it.
+For nums[2]=2 there exist one smaller number than it (1). 
+For nums[3]=2 there exist one smaller number than it (1). 
+For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+Example 2:
+
+Input: nums = [6,5,4,8]
+Output: [2,1,0,3]
+Example 3:
+
+Input: nums = [7,7,7,7]
+Output: [0,0,0,0]
+ * */
+public class M9
+{
+	public static void main(String[] args)
+	{
+		int nums[] = {8,1,2,2,3};
+		int result[] = new int[nums.length];
+		
+		for(int i=0;i<nums.length;i++)
+		{
+			
+			int each=nums[i];
+			int count=0;
+			for(int j=0;j<nums.length;j++)
+			{
+				if(nums[j]<each)
+				{
+					count++;
+				}
+			}
+			result[i]=count;
+		}
+		System.out.println("Final Array :"+Arrays.toString(result));
+	}
+}
